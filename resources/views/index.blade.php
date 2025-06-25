37b982423a32683104c62bf72008e78a8ad18b03
José Eduardo <jose.edu.vs516@gmail.com>
2025-06-25 16:56:49 -0300

    init

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.base')
+
+@section('content')
+
+    <div>
+        <h1>
+            jogos em destaque
+        </h1>
+    </div>
+
+    <div>
+        <h1>
+            categorias
+        </h1>
+    </div>
+
+    <div>
+        <h1>
+            categorias
+        </h1>
+    </div>
+
+@endsection
