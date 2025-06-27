8079bc89165b82982830a75f2aef0de5ebc34752
José Eduardo <jose.edu.vs516@gmail.com>
2025-06-27 17:29:22 -0300

    game page fixes

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -59,7 +59,7 @@
                     <li class="my-4">
                         <span class="flex justify-between items-center">
                             <p>Dificuldade</p>
-                            <p class="text-blue-700 font-semibold text-xl">2.3%</p>
+                            <p class="text-blue-700 font-semibold text-xl">8.7/10</p>
                         </span>
                     </li>
                 </ul>
@@ -493,15 +493,141 @@
 
             <div>
                 <h1 id="platina" class="font-bold text-4xl mb-5">Guias da platina</h1>
-                <p class="mb-2">Elden Ring é um jogo de RPG de ação desenvolvido pela FromSoftware e publicado pela Bandai Namco Entertainment. O jogo é dirigido por Hidetaka Miyazaki e conta com a colaboração do escritor George R. R. Martin.</p>
-                <p class="mb-5">Elden Ring possui múltiplos finais, o que significa que você precisará de várias partidas ou usar salvamentos estratégicos para obter todos os troféus relacionados aos finais sem ter que completar o jogo várias vezes do zero.</p>
+                
+                <div class="flex justify-around">
+                    <div class="flex flex-col justify-around items-start w-96 p-6 bg-white border border-gray-200 shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                        <a href="#">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Elden Ring 100% Walkthrough</h5>
+                        </a>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Guia completo passo a passo para zerar e conseguir todas as conquistas de Elden Ring</p>
+                        <span class="flex justify-start items-center mb-3">
+                            <p class="font-normal text-gray-700 dark:text-gray-400">Por PedroGames23</p>
+                            <img src="{{ asset('images/icos/ico-ps.png') }}" alt="" class="size-4 ms-2">
+                        </span>
+                        <span class="flex justify-start items-center mb-3">
+                            <p class="me-1">10k</p>
+                            <img src="{{ asset('images/icos/ico-like.png') }}" alt="" class="size-5">
+                        </span>
+                        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Ler artigo
+                            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                            </svg>
+                        </a>
+                    </div>
+
+                    <div class="flex flex-col justify-around items-start w-96 mx-4 p-6 bg-white border border-gray-200 shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                        <a href="#">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Guia Speed Run 100% Elden Ring</h5>
+                        </a>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Guia completo da Speed Run de Elden Ring para a categoria "All Achievements"</p>
+                        <span class="flex justify-start items-center mb-3">
+                            <p class="font-normal text-gray-700 dark:text-gray-400">Por JulioSpeedRuns</p>
+                            <img src="{{ asset('images/icos/ico-steam.png') }}" alt="" class="size-4 ms-2">
+                        </span>
+                        <span class="flex justify-start items-center mb-3">
+                            <p class="me-1">2k</p>
+                            <img src="{{ asset('images/icos/ico-like.png') }}" alt="" class="size-5">
+                        </span>
+                        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Ler artigo
+                            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                            </svg>
+                        </a>
+                    </div>
+
+                    <div class="flex flex-col justify-around items-start w-96 p-6 bg-white border border-gray-200 shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                        <a href="#">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Dicas platina raiz - Elden Ring</h5>
+                        </a>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Conjuntos de dicas para facilitar a platina de Elden Ring, mas sem te dar tudo de mão beijada</p>
+                        <span class="flex justify-start items-center mb-3">
+                            <p class="font-normal text-gray-700 dark:text-gray-400">Por CarlosHardcore</p>
+                            <img src="{{ asset('images/icos/ico-ps.png') }}" alt="" class="size-4 ms-2">
+                        </span>
+                        <span class="flex justify-start items-center mb-3">
+                            <p class="me-1">1.3k</p>
+                            <img src="{{ asset('images/icos/ico-like.png') }}" alt="" class="size-5">
+                        </span>
+                        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Ler artigo
+                            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+
                 <div class="bg-gray-600 px-5 mb-8 h-px mt-2 ms-1"></div>
             </div>
 
             <div>
                 <h1 id="guias" class="font-bold text-4xl mb-5">Outros guias</h1>
-                <p class="mb-2">Elden Ring é um jogo de RPG de ação desenvolvido pela FromSoftware e publicado pela Bandai Namco Entertainment. O jogo é dirigido por Hidetaka Miyazaki e conta com a colaboração do escritor George R. R. Martin.</p>
-                <p class="mb-5">Elden Ring possui múltiplos finais, o que significa que você precisará de várias partidas ou usar salvamentos estratégicos para obter todos os troféus relacionados aos finais sem ter que completar o jogo várias vezes do zero.</p>
+                <div class="flex justify-around">
+                    <div class="flex flex-col justify-around items-start w-96 p-6 bg-white border border-gray-200 shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                        <a href="#">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Dicas Bosses Elden Ring</h5>
+                        </a>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Conjutos de dicas para ajudar no combate de todos os Bosses do Elden Ring</p>
+                        <span class="flex justify-start items-center mb-3">
+                            <p class="font-normal text-gray-700 dark:text-gray-400">Por BossRusher</p>
+                            <img src="{{ asset('images/icos/ico-xbox.png') }}" alt="" class="size-4 ms-2">
+                        </span>
+                        <span class="flex justify-start items-center mb-3">
+                            <p class="me-1">5k</p>
+                            <img src="{{ asset('images/icos/ico-like.png') }}" alt="" class="size-5">
+                        </span>
+                        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Ler artigo
+                            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                            </svg>
+                        </a>
+                    </div>
+
+                    <div class="flex flex-col justify-around items-start w-96 mx-4 p-6 bg-white border border-gray-200 shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                        <a href="#">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Guia Speed Run Any% Elden Ring</h5>
+                        </a>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Guia completo da Speed Run de Elden Ring para a categoria "Any%"</p>
+                        <span class="flex justify-start items-center mb-3">
+                            <p class="font-normal text-gray-700 dark:text-gray-400">Por JulioSpeedRuns</p>
+                            <img src="{{ asset('images/icos/ico-steam.png') }}" alt="" class="size-4 ms-2">
+                        </span>
+                        <span class="flex justify-start items-center mb-3">
+                            <p class="me-1">4k</p>
+                            <img src="{{ asset('images/icos/ico-like.png') }}" alt="" class="size-5">
+                        </span>
+                        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Ler artigo
+                            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                            </svg>
+                        </a>
+                    </div>
+
+                    <div class="flex flex-col justify-around items-start w-96 p-6 bg-white border border-gray-200 shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                        <a href="#">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Guia de Builds Elden Ring</h5>
+                        </a>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Conjuntos de diversas Builds prontas para Elden Ring</p>
+                        <span class="flex justify-start items-center mb-3">
+                            <p class="font-normal text-gray-700 dark:text-gray-400">Por AnaRPG555</p>
+                            <img src="{{ asset('images/icos/ico-xbox.png') }}" alt="" class="size-4 ms-2">
+                        </span>
+                        <span class="flex justify-start items-center mb-3">
+                            <p class="me-1">2.7k</p>
+                            <img src="{{ asset('images/icos/ico-like.png') }}" alt="" class="size-5">
+                        </span>
+                        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Ler artigo
+                            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
             </div>
 
         </div>
